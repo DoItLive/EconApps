07695d94565f27d540ebc1a25b453714cf4968ae
Christian Weigandt <weigac10@highpoint.edu>
2012-09-04 14:06:15 -0400

    Added Connection.h/.m and made loginView.h/.m

---
+++ EconApps/loginView.h
@@ -0,0 +1,13 @@
+//
+//  loginView.h
+//  EconApps
+//
+//  Created by Christian Weigandt on 9/4/12.
+//  Copyright (c) 2012 High Point Univeristy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface loginView : UIView
+
+@end
