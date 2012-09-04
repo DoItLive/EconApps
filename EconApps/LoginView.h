ac9637f8ab46ea17504d0531b72c6be9ddcadbb3
Kevin Sanders <Kevin@Kevins-MacBook-Pro.local>
2012-09-04 14:48:27 -0400

    Added login view files

---
+++ EconApps/LoginView.h
@@ -0,0 +1,13 @@
+//
+//  LoginView.h
+//  EconApps
+//
+//  Created by Kevin Sanders on 9/4/12.
+//  Copyright (c) 2012 High Point Univeristy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginView : UIView
+
+@end
