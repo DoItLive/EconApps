6839192e08efc6dd665549e657eae9a6d579e54d
Christian Weigandt <weigac10@highpoint.edu>
2012-09-10 15:55:04 -0400

    Added gridView.  Still not working.

--- EconApps/PublicGoodsView.m
+++ EconApps/PublicGoodsView.m
@@ -14,13 +14,32 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        NSMutableArray *array1 = [[NSMutableArray alloc] initWithObjects:0,1,2,3,4, nil];
-        NSMutableArray *array2 = [[NSMutableArray alloc] initWithObjects:0,5,2,3,4, nil];
-        NSMutableArray *array3 = [[NSMutableArray alloc] initWithObjects:0,6,2,3,4, nil];
-        NSMutableArray *array4 = [[NSMutableArray alloc] initWithObjects:0,7,2,3,4, nil];
-        NSMutableArray *data = [[NSMutableArray alloc] initWithObjects:array1,array2,array3,array4, nil];
+        self.backgroundColor = [UIColor purpleColor];
+        
+        //Test to fill table with junk
+        NSNumber* one = [[NSNumber alloc] initWithInt:1];
+        NSNumber* two = [[NSNumber alloc] initWithInt:2];
+        NSNumber* three = [[NSNumber alloc] initWithInt:3];
+        NSNumber* four = [[NSNumber alloc] initWithInt:4];
+        NSNumber* five = [[NSNumber alloc] initWithInt:5];
+        NSNumber* six = [[NSNumber alloc] initWithInt:6];
+        NSNumber* seven = [[NSNumber alloc] initWithInt:7];
 
-        gridView = [[GridView alloc] initWithFrame:CGRectMake(0, 0, 400, 200) andData:data];
+        NSMutableArray *array1 = [[NSMutableArray alloc] initWithObjects:one,two,three,four,five,six,seven, nil];
+        NSMutableArray *array2 = [[NSMutableArray alloc] initWithObjects:two,two,three,four,five,six,seven, nil];
+        NSMutableArray *array3 = [[NSMutableArray alloc] initWithObjects:three,two,three,four,five,six,seven, nil];
+        NSMutableArray *array4 = [[NSMutableArray alloc] initWithObjects:four,two,three,four,five,six,seven, nil];
+        NSMutableArray *array5 = [[NSMutableArray alloc] initWithObjects:five,two,three,four,five,six,seven, nil];
+        NSMutableArray *array6 = [[NSMutableArray alloc] initWithObjects:six,two,three,four,five,six,seven, nil];
+        NSMutableArray *array7 = [[NSMutableArray alloc] initWithObjects:seven,two,three,four,five,six,seven, nil];
+        NSMutableArray *array8 = [[NSMutableArray alloc] initWithObjects:one,three,three,four,five,six,seven, nil];
+        NSMutableArray *array9 = [[NSMutableArray alloc] initWithObjects:two,three,three,four,five,six,seven, nil];
+        NSMutableArray *array10 = [[NSMutableArray alloc] initWithObjects:three,three,three,four,five,six,seven, nil];
+        NSMutableArray *array11 = [[NSMutableArray alloc] initWithObjects:four,three,three,four,five,six,seven, nil];
+        NSMutableArray *array12 = [[NSMutableArray alloc] initWithObjects:five,three,three,four,five,six,seven, nil];
+        NSMutableArray *data = [[NSMutableArray alloc] initWithObjects:array1,array2,array3,array4,array5,array6,array7,array8,array9,array10,array11,array12, nil];
+
+        gridView = [[GridView alloc] initWithFrame:CGRectMake(10, 10, 400, 200) andData:data];
         [self addSubview:gridView];
 
     }
