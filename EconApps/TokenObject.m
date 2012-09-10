fe069beffcd2d5eaeec4be4ea20a837ffab6dba3
Kevin Sanders <Kevin@Kevins-MacBook-Pro.local>
2012-09-10 15:57:17 -0400

    added token object files

---
+++ EconApps/TokenObject.m
@@ -0,0 +1,13 @@
+//
+//  TokenObject.m
+//  EconApps
+//
+//  Created by Kevin Sanders on 9/7/12.
+//  Copyright (c) 2012 High Point Univeristy. All rights reserved.
+//
+
+#import "TokenObject.h"
+
+@implementation TokenObject
+
+@end
